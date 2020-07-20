e6eef8ff60537c10696ad2100d4870c186bb7e9d
xetxezarreta <xabi1500@gmail.com>
2020-07-20 08:51:37 +0200

    msi decmo

--- decmopy/decmo/msi_test.py
+++ decmopy/decmo/msi_test.py
@@ -5,13 +5,23 @@ from jmetal.core.problem import FloatProblem
 from jmetal.core.solution import FloatSolution
 from decmo import DECMO
 
-SPEED = [i for i in range(10)]
-
 def speed_to_caudal(speed):
    return speed * 20
 
+def speeds_to_caudal(speeds: List[int]):
+   caudal = 0
+   for s in speeds:
+      caudal += speed_to_caudal(s)
+   return caudal
+
 def speed_to_consumption(speed):
    return speed * 10
+
+def speeds_to_consumption(speeds: List[int]):
+   consumption = 0
+   for s in speeds:
+      consumption += speed_to_consumption(s)
+   return consumption
 
 class Compressor(object):
    """Compresor.
@@ -19,7 +29,9 @@ class Compressor(object):
    Attributes:
       id                Número del Compresor (identificador).
       variable_speed    Variable binaria que dice si el compresor permite velocidad variable o no.
-      speed             Velocidad en Hz a la que funciona el compresor (regulable, discretizado) (4bit).
+      speed             Velocidad a la que funciona el compresor (regulable, discretizado).
+      max_speed         Velocidad máxima ala que funciona el compresor (regulable, discretizado).
+      min_speed         Velocidad máxima ala que funciona el compresor (regulable, discretizado).
       h_func            Horas de Funcionamiento desde último mantenimiento.
       h_func_obj        Horas de Funcionamiento Objetivo antes del Mantenimiento.
       f_mtmto           Fecha de Mantenimiento Programado (timestamp).
@@ -32,6 +44,8 @@ class Compressor(object):
       id: int,
       variable_speed: int,
       speed: int,
+      max_speed: int,
+      min_speed: int,
       h_func: float,
       h_func_obj: float,
       f_mtmto: float,
@@ -39,6 +53,8 @@ class Compressor(object):
       self.id = id
       self.variable_speed = variable_speed
       self.speed = speed
+      self.max_speed = max_speed
+      self.min_speed = min_speed
       self.h_func = h_func
       self.h_func_obj = h_func_obj
       self.f_mtmto = 3
@@ -48,15 +64,14 @@ class Compressor(object):
 
 
 class MSI(FloatProblem):
-   def __init__(self, compressors: List[Compressor]):
+   def __init__(self):
       super(MSI, self).__init__()
-      self.compressors = compressors
-      self.number_of_variables = len(compressors)
-      self.number_of_objectives = 3
+      self.compressors = []
+      self.number_of_variables = 0
+      self.number_of_objectives = 4
       self.number_of_constraints = 0
-
-      self.lower_bound = self.number_of_variables * [min(SPEED)]
-      self.upper_bound = self.number_of_variables * [max(SPEED)]
+      self.lower_bound = []
+      self.upper_bound = []
 
    def evaluate(self, solution: FloatSolution) -> FloatSolution:      
       # obj1: Minimizar la suma de los consumos de todos los compresores
@@ -66,62 +81,73 @@ class MSI(FloatProblem):
          consumption += speed_to_consumption(rounded_speed)
       solution.objectives[0] = consumption
 
-      # obj2: Minimizar Cambios desde la solucion anterior
+      # obj2: Maximizar la distribución de horas de funcionamiento 
+      avg_useful_life = 0
+      for i, speed in enumerate(solution.variables):
+         rounded_speed = int(round(speed))
+         if speed != 0:
+            avg_useful_life += self.compressors[i].avg_useful_life
+      solution.objectives[1] = -1.0 * avg_useful_life
+
+      # obj3: Minimizar Cambios desde la solucion anterior
       changes = 0
       for i, speed in enumerate(solution.variables):
          rounded_speed = int(round(speed))
          if rounded_speed != self.compressors[i].speed:
             changes += 1
-      solution.objectives[1] = changes   
+      solution.objectives[2] = changes  
 
-      # obj3: Maximizar la diferencia de caudal respecto a la solución anterior
+      # obj4: Maximizar la diferencia de caudal respecto a la solución anterior
       caudal_diff = sum(i.caudal for i in self.compressors)
       for i, speed in enumerate(solution.variables):
          rounded_speed = int(round(speed))
          caudal_diff -= speed_to_caudal(rounded_speed)
-      solution.objectives[2] = caudal_diff     
+      solution.objectives[3] = caudal_diff     
 
       return solution 
-
-   def create_solution(self) -> FloatSolution:
-      new_solution = FloatSolution(
-         self.lower_bound,
-         self.upper_bound,
-         self.number_of_objectives,
-         self.number_of_constraints,
-      )      
-      new_solution.number_of_variables = self.number_of_variables
-
-      new_solution.variables = \
-         [random.randint(min(SPEED), max(SPEED)) for _ in range(self.number_of_variables)]    
-
-      return new_solution
+   
+   def add_compressor(self, compressor: Compressor):
+      self.compressors.append(compressor)
+      self.number_of_variables += 1
+      self.upper_bound.append(compressor.max_speed)
+      self.lower_bound.append(compressor.min_speed)   
 
    def get_name(self) -> str:
       return "MSI"
 
 def main():
-   var_speed = 1
-   speed = [0, 1, 2, 3]
-   h_func = [50, 100, 150, 200]
    h_func_obj = 250
    f_mtmto = time.mktime(datetime.datetime.strptime("01/07/2020", "%d/%m/%Y").timetuple())
-
-   compressors = []
-   for i in range(4):
-      comp = Compressor(i, var_speed, speed[i], h_func[i], h_func_obj, f_mtmto)
-      compressors.append(comp)
    
-   problem = MSI(compressors)
+   problem = MSI()
+   problem.add_compressor(Compressor(1, 0, 1, 1, 0, 100, h_func_obj, f_mtmto))
+   problem.add_compressor(Compressor(2, 0, 1, 1, 0, 100, h_func_obj, f_mtmto))
+   problem.add_compressor(Compressor(3, 1, 2, 9, 0, 200, h_func_obj, f_mtmto))
+   problem.add_compressor(Compressor(4, 1, 2, 9, 0, 200, h_func_obj, f_mtmto))
+
    algorithm = DECMO(problem, individual_population_size=50, max_iterations=100)
    results = algorithm.run()
    print(f"Algorithm: ${algorithm.get_name()}")
    print(f"Problem: ${problem.get_name()}")
    print(f"Final non-dominted solution set size: ${len(results)}")
 
-   for res in results:
-      print(res.variables)
+   global_caudal = 0
+   global_consumption = 0
+   for c in problem.compressors:
+      if c.speed != 0:
+         global_caudal += speed_to_caudal(c.speed)
+         global_consumption += speed_to_consumption(c.speed)
 
+   final_solutions = []
+
+   for result in results:
+      s = [int(round(i)) for i in result.variables]
+      caudal = speeds_to_caudal(s)
+      consumption = speeds_to_consumption(s)
+
+      if global_caudal <= caudal and s not in final_solutions:
+         final_solutions.append(s)
+         print(s, "Caudal Global: ", str(global_caudal), "Caudal Solución: ", str(caudal), "Consumo Global: ", str(global_consumption), "Consumo Solución: ", str(consumption))  
 
 if __name__ == "__main__":
     main()
